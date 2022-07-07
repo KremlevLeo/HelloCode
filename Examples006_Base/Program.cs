3e309a62daf9f77651c8fc8b8446504c6c2f15d6
Leonid <leeonidix@gmail.com>
2022-07-08 00:05:25 +0500

    Add Example 007 Magic

--- Examples006_Base/Program.cs
+++ Examples006_Base/Program.cs
@@ -6,10 +6,9 @@ int e = 4;
 
 int max = a;
 
-if(b > max) max = b;
-if(c > max) max = c;
-if(d > max) max = d;
-if(e > max) max = e;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
 
 Console.WriteLine($"max = {max}");
-
