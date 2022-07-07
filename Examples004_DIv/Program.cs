dbbd4a28ad00b4cc2582a9c5f2bdab51662ea003
Leonid <leeonidix@gmail.com>
2022-07-07 22:57:08 +0500

    Div upgrate

--- Examples004_DIv/Program.cs
+++ Examples004_DIv/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double numbrA = new Random().Next(1,10);
+Console.WriteLine(numbrA);
+double numbrB = new Random().Next(1,10);
+Console.WriteLine(numbrB);
+double result = numbrA/numbrB;
+Console.WriteLine(result);
